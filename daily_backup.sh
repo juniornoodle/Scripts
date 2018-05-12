c5a36bd27a38756e2fc05116c864012f2709aeb1
Michael McCaffery <mmccaffery@mmccaffery.wks.liquidweb.com>
2018-05-12 15:20:07 -0400

    A simple daily backup script for CentOS

---
+++ daily_backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Define source directory
+sourcedir="/home/mmccaffery/"
+
+#Get the time
+time=`date "+%Y.%m.%d"`
+
+#Create backupdir
+backupdir="/backup/daily-$time"
+mkdir $backupdir
+
+#Start the backup
+rsync -avHP $sourcedir $backupdir
+
+#Compress the backup
+tar -czvf "$backupdir.tar.gz" $backupdir
+
+#Clean up uncompressed backup
+rm -rf $backupdir
+
+#Remove any backups older than 30 days
+find "/backup/" -type f -mtime +30 -exec rm -rf {} \;
